808d3b76f46d56fa6878fc38c7f48eb66e9d34e1
Sara Serti <saaris1985@gmail.com>
2024-11-23 17:14:56 +0100

    keydown func

---
+++ keydownfunc.js
@@ -0,0 +1,8 @@
+document.addEventListener("DOMContentLoaded", function() {
+
+  const secretWord = "javascript"; //vårt hemliga ord, SLUMPA HÄR SEN!!
+  const wordDisplay = document.querySelector(".word-display");
+  const guessedLettersDisplay = document.querySelector(".show-guessed-letters");
+  const letterInput = document.getElementById("letter-input");
+  
+ 
