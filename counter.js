7267339c854331acdaf9932fbfd335bb90395fcd
sally <sally1994lindgren@gmail.com>
2024-11-23 23:18:19 +0100

    decomposition

--- counter.js
+++ counter.js
@@ -11,5 +11,35 @@ function updateScore(points) {
 
 console.log(updateScore(10));
 
+//svg-parts
+
+const parts = [
+	document.querySelector('#ground'),
+	document.querySelector('#scaffold'),
+	document.querySelector('#head'),
+	document.querySelector('#body'),
+	document.querySelector('#arms'),
+	document.querySelector('#legs')
+]
+
+let attempts = 6;
 
 
+
+
+
+/* Score-style-page
+
+name = input.value
+
+const result = document.querySelector('#result');
+const win = document.createElement('p');
+const lose = document.createElement ('p');
+
+win.innerText = ` Wins: ${}`;
+lose.innerText = `Loses: ${}`;
+
+result.appendChild(win);
+result.appendChild(lose);
+
+*/
