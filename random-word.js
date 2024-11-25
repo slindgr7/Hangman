398dd0aaee99387a792f5207b22580b92a68ea70
Akevonwolfe <94.andreas.svensson@gmail.com>
2024-11-25 22:29:39 +0100

    tried to get the random word to work, no luck

--- random-word.js
+++ random-word.js
@@ -1,6 +1,9 @@
 import { words } from './swedish-words.js';
 
 function getRandomWord() {
+    if(words.length === 0 ){
+        console.log("Empty")
+    }
     const randomIndex = Math.floor(Math.random() * words.length);
     const randomWord = words[randomIndex];
     console.log("does it work", randomWord);
