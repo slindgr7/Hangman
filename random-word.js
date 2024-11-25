d9e87048ac7bb0f7d7d05767ae4372081d60ad41
Akevonwolfe <94.andreas.svensson@gmail.com>
2024-11-25 19:14:43 +0100

    merge

---
+++ random-word.js
@@ -0,0 +1,10 @@
+import { words } from './swedish-words.js';
+
+function getRandomWord() {
+    const randomIndex = Math.floor(Math.random() * words.length);
+    const randomWord = words[randomIndex];
+    console.log("does it work", randomWord);
+    return randomWord;
+}
+//if else for word string lenght
+export  { getRandomWord };
