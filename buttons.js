6e56744542b2e041bb64eb2c0fbdf5da280aef87
Akevonwolfe <94.andreas.svensson@gmail.com>
2024-11-29 09:46:59 +0100

    forgot a button

--- buttons.js
+++ buttons.js
@@ -4,8 +4,7 @@ const playButtonlose = document.querySelector('.gameover-button')
 const playButtonwin = document.querySelector('.win-button')
 const scoreButtonLose = document.querySelector('.gameover-score-button')
 const scoreButtonwin = document.querySelector('.win-score-button')
-const showScoreBtn = document.querySelector('#show-score')
-const closeScoreBtn = document.querySelector('#close-score')
+const ScoreButtonHomescreen = document.querySelector('.hoem-screen-score-button')
 const dialog = document.querySelector('dialog')
 
 playButtonlose.addEventListener('click', function() {
@@ -20,6 +19,11 @@ playButtonwin.addEventListener('click', function() {
 
 dialog.addEventListener('click', event => {
         event.stopPropagation()
+    })
+
+ScoreButtonHomescreen.addEventListener('click', () => {
+        scoreContainer.classList.remove('hide')
+        dialog.show()
     })
 scoreButtonLose.addEventListener('click', () => {
     scoreContainer.classList.remove('hide')
